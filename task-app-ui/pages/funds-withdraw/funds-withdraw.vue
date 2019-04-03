b9b8daf4583fc60b142f79d8135b995517879639
WangZhenyu <847315251@qq.com>
2019-04-03 15:05:18 +0800

    1、增加微信二维码图片选择 2、提现申请表单和表单验证，使用graceui的表单验证功能

--- task-app-ui/pages/funds-withdraw/funds-withdraw.vue
+++ task-app-ui/pages/funds-withdraw/funds-withdraw.vue
@@ -1,23 +1,52 @@
 <template>
-	<view>
-		提现
+	<view class="zy-page">
+		<view class="zy-form">
+			<view class="uni-form-item uni-column">
+				<view class="title">可提现积分：1000</view>
+				<input v-model="withdrawForm.integral" class="uni-input" type="number" placeholder="请输入提现积分" />
+			</view>
+			<view class="uni-form-item">
+				<button type="primary" style="width: 100%;" @click="submitWithdraw">申请提现</button>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	const graceChecker = require("../../common/graceChecker.js");
 	export default {
 		data() {
 			return {
-				
+				withdrawForm: {
+					integral: null
+				}
 			}
 		},
 		onLoad() {
 		},
 		methods: {
+			submitWithdraw() {
+				const rule = [
+					{name:'integral', checkType : 'betweenD', checkRule: '1,1000',  errorMsg:'请输入1-' + '1000的整数积分'}
+				]
+				const checkRes = graceChecker.check(this.withdrawForm, rule);
+				if(checkRes){
+					uni.showToast({title:"验证通过!", icon:"none"});
+				}else{
+					uni.showToast({ title: graceChecker.error, icon: "none" });
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
 	@import '../../common/zywork-main.scss';
+	.zy-page {
+		width: 100%;
+		padding-top: 20upx;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 </style>
--- task-app-ui/pages/weixin-barcode/weixin-barcode.vue
+++ task-app-ui/pages/weixin-barcode/weixin-barcode.vue
@@ -1,6 +1,7 @@
 <template>
-	<view>
-		微信二维码
+	<view class="zy-page">
+		<image :src="weixinBarcode"/>
+		<text class="zy-text-info-strong" @click="chooseImage">上传我的微信二维码</text>
 	</view>
 </template>
 
@@ -8,16 +9,43 @@
 	export default {
 		data() {
 			return {
-				
+				weixinBarcode: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/doc/img/ad.png'
 			}
 		},
 		onLoad() {
 		},
 		methods: {
+			chooseImage() {
+				uni.chooseImage({
+					sizeType: ['original', 'compressed'],
+					sourceType: ['album'], 
+					count: 1,
+					success: (res) => {
+						this.weixinBarcode = res.tempFilePaths
+						
+					},
+					fail: (res) => {
+						console.log(res)
+					}
+				})
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
 	@import '../../common/zywork-main.scss';
+	
+	.zy-page {
+		padding-top: 20upx;
+		padding-left: 10upx;
+		padding-rihgt: 10upx;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	
+	.zy-page image {
+		margin-bottom: 50upx;
+	}
 </style>
